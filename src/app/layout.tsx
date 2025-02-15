41b23e4c1d186a9a106aaf34be462be7783b6f7e
Suman mondal <smondal.qwerty@gmail.com>
2025-02-15 15:57:54 +0530

    resolved merge errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import '../styles/index.tsx';
-import Navbar from '@/components/navbar';
 import { Poppins } from 'next/font/google'
 import Footer from '@/components/footer';
 import NavbarPc from '@/components/navbar/navbarPc';
@@ -11,16 +10,13 @@ const poppins = Poppins({
   subsets: ['latin'],
   weight: "200",
 });
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
   return (
     <html lang="en">
       <body className={`${poppins.className} mt-20`}>
         <NavbarPc />
         <NavbarMobile />
+        <Hero/>
         {children}
         <Footer />
         <ToTopButton />
