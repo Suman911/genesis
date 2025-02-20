f421e0109255b35950600cc55f5b0b44ba6016f8
Soumik <soumik20020906+github@gmail.com>
2025-02-20 12:46:46 +0530

    added error text

--- src/lib/regexs.tsx
+++ src/lib/regexs.tsx
@@ -1,4 +1,4 @@
-export const regexs =
+export const regexs: { [key: string]: RegExp }  =
 {
     name: /^[a-zA-Z]{2,30}(?:\s[a-zA-z]{2,30})?(?:\s[a-zA-Z]{2,30})?$/,
     email: /^[a-z0-9][a-z0-9-_.]+@([a-z]|[a-z0-9]?[a-z0-9-]+[a-z0-9]).[a-z0-9]{2,10}(?:.[a-z]{2,10})?$/
