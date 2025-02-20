2af5fa80b3e2f2cf855e7280accffa809e965343
Soumik <soumik20020906+github@gmail.com>
2025-02-20 09:39:19 +0530

    added color coding of ragex match

--- src/components/ui/form/textbox.tsx
+++ src/components/ui/form/textbox.tsx
@@ -16,24 +16,40 @@ export function TextBox({ children, className = "", ...props }: text) {
     const [value, setValue] = useState("")
     const txtbox = useRef<HTMLInputElement | null>(null);
     return (
-        <div className={`w-full py-4 px-0 sm:py-4 ${className}`}>
-            <input ref={txtbox} value={value} onChange={
-                (e) => {
-                    setValue(e.target.value)
-                    const curr=txtbox.current
-                    const id:string=curr?.id||'';
-                    if (Object.keys(regexs).includes(id)) {
-                        // check regex
-                        const regex=regexs[id]
-                        if(value.match(regex)){
-                           console.log('Regex matches')
-                        }
-                        else{
-                            console.log('Regex doesn\'t matches')
+        <div className={`w-full py-4 px-0 sm:py-4  ${className}`}>
+            <input ref={txtbox} value={value}
+                onChange={
+                    (e) => {
+                        setValue(e.target.value)
+                    }
+                }
+                onBlur={
+                    () => {
+                        const curr = txtbox.current
+                        const id = curr?.id || '';
+                        if (Object.keys(regexs).includes(id)) {
+                            // check regex
+                            const regex = regexs[id]
+                            if (value.match(regex)) {
+                                if(curr?.classList.contains('!border-red-600')){
+                                    curr?.classList.remove('!border-red-600');
+                                }
+                                if(!curr?.classList.contains('!border-green-400')){
+                                    curr?.classList.add('!border-green-400');
+                                }
+                            }
+                            else {
+                                if(curr?.classList.contains('!border-green-400')){
+                                    curr?.classList.remove('!border-green-400');
+                                }
+                                if(!curr?.classList.contains('!border-red-600')){
+                                    curr?.classList.add('!border-red-600');
+                                }
+                            }
                         }
                     }
                 }
-            } {...props} className="w-full p-2 border-2 border-primary rounded-xl" type="text" />
+                {...props} className="w-full p-2 outline-none border-2 border-primary rounded-xl" type="text" />
             {children}
         </div>
     );
