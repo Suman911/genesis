14c3d4ac56f64401ee4a38366c19e38df5c3277a
Suman mondal <smondal.qwerty@gmail.com>
2025-04-02 00:20:00 +0530

    updated navbar styling

--- src/components/navbar/navbarMobile.tsx
+++ src/components/navbar/navbarMobile.tsx
@@ -1,56 +1,91 @@
-"use client"
+"use client";
 
 import { navs } from "@/lib/navs";
 import clsx from "clsx";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FiAlignJustify } from "react-icons/fi";
 import { TfiClose } from "react-icons/tfi";
-export default function NavbarMobile({ }) {
+
+export default function NavbarMobile() {
     const pathname = usePathname();
     const [dropmenu, setDropmenu] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 10);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
-        <>
-            <div className="p-4 z-50 w-full bg-primary text-white fixed top-0 shadow-lg block lg:hidden">
-                <div className="flex justify-between">
-                    <Link href="/">
-                        <img className="size-12 ml-4 my-auto" src="/assets/images/logo_main.png" alt="logo" />
-                    </Link>
-                    <div className="hamburger h-10 w-10">
-                        <button onClick={
-                            () => {
-                                setDropmenu(!dropmenu);
-                            }
-                        } className="h-full w-full focus:outline-none text-3xl">
-                            {dropmenu ? <TfiClose/> : <FiAlignJustify />}
-                        </button>
-                    </div>
-                </div>
-                <div className={`overflow-hidden transition-all duration-300 ease-linear  ${dropmenu ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0'}`}>
-                    <ul className="flex p-4
-                flex-col">
-                        {
-                            navs.map(
-                                (nav) => (
-                                    <li key={nav.name} className={clsx("m-2 p-3 cursor-pointer hover:bg-slate-700 rounded-lg text-lg",
-                                        {
-                                            "bg-slate-700 text-orange-400": pathname == nav.path
-                                        }
-                                    )}>
-                                        <Link href={nav.path} className="flex w-full h-full items-center gap-4" onClick={() => {
-                                            setDropmenu(!dropmenu);
-                                        }}>
-                                            {nav.icon}{nav.name}
-                                        </Link>
-                                    </li>
-                                )
-                            )
-                        }
-                        <li><button className="bg-blue-600  p-2 rounded-lg hover:bg-blue-700 shadow-blue-600/50 shadow-lg text-lg">Notice Board</button></li>
-                    </ul>
+        <div
+            className={clsx(
+                "p-4 z-50 w-full text-white fixed top-0 backdrop-blur-md shadow-lg block lg:hidden transition-all duration-500",
+                isScrolled ? "bg-primary/80 shadow-lg" : "bg-black/30"
+            )}
+        >
+            <div className="flex justify-between items-center">
+                {/* Logo */}
+                <Link href="/">
+                    <img
+                        className="size-12 ml-4"
+                        src="/assets/images/logo_main.png"
+                        loading="lazy"
+                        alt="logo"
+                    />
+                </Link>
+
+                {/* Hamburger Menu */}
+                <div className="hamburger h-10 w-10">
+                    <button
+                        onClick={() => setDropmenu(!dropmenu)}
+                        className="h-full w-full focus:outline-none text-3xl"
+                    >
+                        {dropmenu ? <TfiClose /> : <FiAlignJustify />}
+                    </button>
                 </div>
             </div>
-        </>
+
+            {/* Dropdown Menu */}
+            <div
+                className={clsx(
+                    "overflow-hidden transition-all duration-300 ease-linear",
+                    dropmenu ? "max-h-screen opacity-100" : "max-h-0 opacity-0"
+                )}
+            >
+                <ul className="flex flex-col p-4">
+                    {navs.map((nav) => (
+                        <Link
+                            href={nav.path}
+                            key={nav.name}
+                            className={clsx(
+                                "m-2 p-3 hover:bg-slate-700/80 rounded-lg text-lg transition-all duration-300",
+                                {
+                                    "bg-slate-800/80 text-orange-400": pathname === nav.path,
+                                }
+                            )}
+                            onClick={() => setDropmenu(false)}
+                        >
+                            <li
+                                className="flex w-full h-full items-center gap-4"
+                            >
+                                {nav.icon} {nav.name}
+                            </li>
+                        </Link>
+                    ))}
+                    {/* Notice Board Button */}
+                    <li>
+                        <button className="bg-blue-600 p-2 rounded-lg hover:bg-blue-700 shadow-blue-600/50 shadow-lg text-lg transition-all duration-300">
+                            Notice Board
+                        </button>
+                    </li>
+                </ul>
+            </div>
+        </div>
     );
 }
--- src/components/navbar/navbarPc.tsx
+++ src/components/navbar/navbarPc.tsx
@@ -3,38 +3,67 @@ import { navs } from "@/lib/navs";
 import clsx from "clsx";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function NavbarPc() {
     const pathname = usePathname();
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 10);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
-        <div className="p-2 z-50 w-full bg-primary text-white fixed top-0 shadow-lg hidden lg:block">
-            <div className="flex justify-between">
+        <div
+            className={clsx(
+                "p-2 z-50 w-full text-white fixed top-0 backdrop-blur-md hidden lg:block transition-all duration-500",
+                isScrolled ? "h-16 bg-primary/80 shadow-lg" : "h-20 bg-black/0"
+            )}
+        >
+            <div className="flex justify-between items-center h-full">
+                {/* Logo */}
                 <Link href="/">
-                    <img className="size-12 ml-4 my-auto" src="/assets/images/logo_main.png" alt="logo" />
+                    <img
+                        className="size-12 ml-4"
+                        src="/assets/images/logo_main.png"
+                        loading="lazy"
+                        alt="logo"
+                    />
                 </Link>
+
+                {/* Navigation Links */}
                 <div className="navigation block">
-                    <ul className="flex space-x-4 h-full m-auto">
-                        {
-                            navs.map(
-                                (nav) => (
-                                    <li key={nav.name} className={clsx("m-2 p-3 cursor-pointer hover:bg-slate-700 rounded-lg text-lg", {
-                                        "bg-slate-800 text-orange-400": pathname == nav.path
-                                    })}>
-                                        <Link href={nav.path} className="w-full h-full block">
-                                            {nav.name}
-                                        </Link>
-                                    </li>
-                                )
-                            )
-                        }
+                    <ul className="flex space-x-4">
+                        {navs.map((nav) => (
+                            <Link
+                                key={nav.name}
+                                href={nav.path}
+                                className={clsx(
+                                    "m-2 p-3 hover:bg-slate-700/80 rounded-lg text-lg transition-all duration-300",
+                                    {
+                                        "bg-slate-800/80 text-orange-400": pathname == nav.path,
+                                    }
+                                )}
+                            >
+                                <li>{nav.name}</li>
+                            </Link>
+                        ))}
                     </ul>
                 </div>
 
-                {/* Notice Button (Desktop) */}
-                <div className="btn my-auto mx-2 block">
-                    <button className="bg-blue-600 p-2 rounded-lg hover:bg-blue-700 shadow-blue-600/50 shadow-lg text-lg">
+                {/* Notice Button */}
+                <div className="btn my-auto mx-2">
+                    <Link
+                        href="/#notice"
+                        className="bg-blue-600 p-2 rounded-lg hover:bg-blue-700 shadow-blue-600/50 shadow-lg text-lg transition-all duration-300"
+                    >
                         Notice Board
-                    </button>
+                    </Link>
                 </div>
             </div>
         </div>
