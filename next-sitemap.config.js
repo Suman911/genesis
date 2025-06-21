494b0f6762b4b6837c8ffb25acd80267e5b34054
Suman911 <smondal.qwerty@gmail.com>
2025-06-21 22:06:44 +0530

    chore: update dependencies and scripts in package.json
    - Updated axios from ^1.7.9 to ^1.10.0
    - Updated @tailwindcss/postcss from ^4.0.6 to ^4.1.10
    - Updated motion from ^12.6.2 to ^12.18.1
    - Updated next from ^15.2.4 to ^15.3.4
    - Updated postcss from ^8.5.2 to ^8.5.6
    - Updated react from ^19.0.0 to ^19.1.0
    - Updated react-dom from ^19.0.0 to ^19.1.0
    - Updated react-icons from ^5.4.0 to ^5.5.0
    - Updated sharp from ^0.33.5 to ^0.34.2
    - Updated swiper from ^11.2.6 to ^11.2.8
    - Updated tailwindcss from ^4.0.6 to ^4.1.10
    - Updated @types/node from ^20 to ^24
    - Updated eslint-config-next from 15.1.6 to 15.3.4
    - Updated tailwind-scrollbar from ^4.0.0 to ^4.0.2
    - Updated typescript from ^5 to ^5.8.3
    
    - Added new scripts: sitemap, post-build, and out
    - Modified build script to include sitemap generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,39 @@
+const siteUrl = process.env.NEXT_PUBLIC_URL;
+console.log('Generating site map for: ', siteUrl);
+if (!siteUrl) {
+    throw new Error('❌ NEXT_PUBLIC_URL is not defined in .env');
+}
+const config = {
+    siteUrl: siteUrl,
+    generateRobotsTxt: true,
+    outDir: './out',
+    changefreq: 'monthly',
+    priority: 0.7,
+    sitemapSize: 5000,
+    exclude: ['/admin', '/admin/*', '/api', '/api/*'],
+    robotsTxtOptions: {
+        policies: [
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: ['/admin', '/admin/*', '/api', '/api/*'],
+            },
+        ],
+    },
+    transform: async (config, path) => {
+        console.log('Path:', path);
+        const custom = {
+            '/': { priority: 1.0 },
+            '/auth/login': { priority: 0.3 },
+            '/story': { priority: 0.9 },
+            '/gallery': { priority: 0.8 },
+        };
+        return {
+            loc: path,
+            changefreq: custom[path]?.changefreq || config.changefreq,
+            priority: custom[path]?.priority || config.priority,
+            lastmod: new Date().toISOString(),
+        };
+    },
+};
+module.exports = config;
